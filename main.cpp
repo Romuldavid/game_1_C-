e136686834e077b09b3135903a897389f75e7ce2
Roman Davydov <romuldavid@mail.ru>
2022-12-12 17:45:39 +0300

    Background+

--- main.cpp
+++ main.cpp
@@ -7,27 +7,27 @@ using namespace sf;
 int main()
 {
     // Create a video mode object
-    //VideoMode vm(1920, 1080);
-    VideoMode vm(640, 480);
+    VideoMode vm(1920, 1080);
+    //VideoMode vm(640, 480);
 
     // Create and open a window for the game
-    //RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
-    RenderWindow window(vm, "Timber!!!");
+    RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+    //RenderWindow window(vm, "Timber!!!");
 
     // Create a texture to hold a graphic on the GPU
-    Texture textureBackground;
+	Texture textureBackground;
 
-    // Load a graphic into the texture
-    textureBackground.loadFromFile("graphics/background.png");
+	// Load a graphic into the texture
+	textureBackground.loadFromFile("graphics/background.png");
 
-    // Create a sprite
-    Sprite spriteBackground;
+	// Create a sprite
+	Sprite spriteBackground;
 
-    // Attach the texture to the sprite
-    spriteBackground.setTexture(textureBackground);
+	// Attach the texture to the sprite
+	spriteBackground.setTexture(textureBackground);
 
-    // Set the spriteBackground to cover the screen
-    spriteBackground.setPosition(0, 0);
+	// Set the spriteBackground to cover the screen
+	spriteBackground.setPosition(0, 0);
 
     while(window.isOpen())
     {
@@ -55,6 +55,7 @@ int main()
        window.clear();
 
        //Draw our game scene here
+       window.draw(spriteBackground);
 
        //
        //Show everything we just drew
