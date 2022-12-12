e82c507e8ca7b2136859c74fc442038a74ab7ab7
Roman Davydov <romuldavid@mail.ru>
2022-12-12 16:05:19 +0300

    Display

--- main.cpp
+++ main.cpp
@@ -7,10 +7,44 @@ using namespace sf;
 int main()
 {
     // Create a video mode object
-    VideoMode vm(1920, 1080);
+    //VideoMode vm(1920, 1080);
+    VideoMode vm(640, 480);
 
     // Create and open a window for the game
-    RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+    //RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+    RenderWindow window(vm, "Timber!!!");
+
+    while(window.isOpen())
+    {
+        //Handle the players input
+        Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == Event::Closed)
+                window.close();
+        }
+
+        /*
+        ****************************************
+        Update the scene
+        ****************************************
+        */      
+
+       /*
+        ****************************************
+        Draw the scene
+        ****************************************
+        */
+
+       //Clear everything from the last frame
+       window.clear();
+
+       //Draw our game scene here
+
+       //
+       //Show everything we just drew
+       window.display();
+    }
 
     return 0;
 }
