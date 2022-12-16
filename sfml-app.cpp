e7c709075d3075d49b7c930be201f56c4ac5bb9d
Roman Davydov <romuldavid@mail.ru>
2022-12-16 13:36:50 +0300

    Add timebar

--- sfml-app.cpp
+++ sfml-app.cpp
@@ -86,6 +86,18 @@ int main()
     // Variables to control time itself
 	Clock clock;
 
+    //Time bar
+    RectangleShape timeBar;
+    float timeBarStarWidth = 400;
+    float timeBarHeight = 80;
+    timeBar.setSize(Vector2f(timeBarStarWidth, timeBarHeight));
+    timeBar.setFillColor(Color::Red);
+    timeBar.setPosition((1920 / 2) - timeBarStarWidth / 2, 980);
+
+    Time gameTimeTotal;
+    float timeRemaining = 6.0f;
+    float timeBarWidthPerSecond = timeBarStarWidth / timeRemaining;
+
     // Track whether the game is running
     bool paused = true;
 
@@ -140,6 +152,10 @@ int main()
             if(Keyboard::isKeyPressed(Keyboard::Return))
             {
                 paused = false;
+
+                // Reset the time and the score
+                score = 0;
+                timeRemaining = 6;
             }
         }
 
@@ -153,6 +169,30 @@ int main()
         //  Measure time
         
         Time dt = clock.restart();
+
+        //Subtract from the amount of time remaining
+        timeRemaining -= dt.asSeconds();
+        //size up the time bar
+        timeBar.setSize(Vector2f(timeBarWidthPerSecond * 
+            timeRemaining, timeBarHeight));
+
+        if(timeRemaining <= 0.0f)
+        {
+            // Pause the game
+            paused = true;
+
+            // Change the message shown to the player
+            messageText.setString("Out of time!!");
+
+            //Reposition the text based on its new size
+            FloatRect textRect = messageText.getLocalBounds();
+            messageText.setOrigin(textRect.left +
+                textRect.width / 2.0f,
+                textRect.top +
+                textRect.height / 2.0f);
+
+            messageText.setPosition(1920 / 2.0f, 1080 / 2.0f);    
+        }
 
         // Setup the bee
         if(!beeActive)
@@ -294,6 +334,9 @@ int main()
        //Draw the score
         window.draw(scoreText);
 
+        // Draw the timebar
+        window.draw(timeBar);
+
         if(paused)
         {
             //Draw our message
@@ -302,6 +345,8 @@ int main()
 
        //Show everything we just drew
        window.display();
+
+       //score++;
     }
 
     return 0;
