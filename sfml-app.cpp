940a89fa845e9795af75d0a97aadafd26761454a
Roman Davydov <romuldavid@mail.ru>
2022-12-22 15:00:21 +0300

    Game complite

--- sfml-app.cpp
+++ sfml-app.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <SFML/Graphics.hpp>
 //#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
 using namespace sf;
 
 //Function declaration
@@ -214,6 +215,25 @@ int main()
     //Control the player input
     bool acceptInput = false;
 
+    //Prepare the sounds
+    // The player chopping sound
+    SoundBuffer chopBuffer;
+    chopBuffer.loadFromFile("sound/chop.wav");
+    Sound chop;
+    chop.setBuffer(chopBuffer);
+
+    //The player has met his end under a branch
+    SoundBuffer deathBuffer;
+    deathBuffer.loadFromFile("sound/death.wav");
+    Sound death;
+    death.setBuffer(deathBuffer);
+
+    //Out of time
+    SoundBuffer ootBuffer;
+    ootBuffer.loadFromFile("sound/out_of_time.wav");
+    Sound outOfTime;
+    outOfTime.setBuffer(ootBuffer);
+
 
     while(window.isOpen())
     {
@@ -309,6 +329,9 @@ int main()
                     logActive = true;
 
                     acceptInput = false;
+
+                    //Play a chop sound
+                    chop.play();
                 }   
             }
         }
@@ -346,6 +369,9 @@ int main()
                 textRect.height / 2.0f);
 
             messageText.setPosition(1920 / 2.0f, 1080 / 2.0f);    
+
+            //Play the out of time sound
+            outOfTime.play();
         }
 
         // Setup the bee
@@ -532,6 +558,9 @@ int main()
 
                 messageText.setPosition(1920 / 2.0f,
                     1080 / 2.0f);
+
+                //Play the death sound
+                death.play();
             }
 
        } //End if(!paused)
